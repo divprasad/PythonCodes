f7f8923d3a9d9e2d323bc85705c779b724daf500
divprasad_Dell <dkp.iitb@gmail.com>
2018-02-18 14:05:42 +0100

    yooo

---
+++ splitBigfasta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#a script that makes a single fasta file
+#takes two arguments as input : (1) filename of the single file to be created
+#and (2) the file extension of the vir or bac files
+
+from Bio import SeqIO
+import sys
+
+file1=sys.argv[1]     #takes the filename of the single file to be created
+k=sys.argv[2]
+count=1
+num=1
+for seq_record in SeqIO.parse(file1, "fasta"):  #iterates through all the sequences in each file
+    if count % k == 0:
+        num=num+1
+    my_file="try_" +str(num)
+    f1= open(my_file, "a")
+    print>>f1,">"+seq_record.id
+    print>>f1,seq_record.seq
+    f1.close()
+    count=count +1
